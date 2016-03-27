a888bc5b9f613777343da00c9f8ed5347aab5c40
xuxiaohongok <749881273@qq.com>
2016-03-27 13:43:54 +0800

    ok

--- src/main/java/com/zhidian3g/nextad/action/TestAction.java
+++ src/main/java/com/zhidian3g/nextad/action/TestAction.java
@@ -1,18 +1,20 @@
 package com.zhidian3g.nextad.action;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.InputStream;
+import java.net.URLDecoder;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.druid.support.json.JSONUtils;
+import com.zhidian3g.common.utils.CryptUtils;
+import com.zhidian3g.nextad.service.AdService;
 
 /**
  * @author Administrator
@@ -21,34 +23,43 @@ import com.alibaba.druid.support.json.JSONUtils;
 @Controller
 public class TestAction {
 	
-	private Logger logger = LoggerFactory.getLogger("es-access");
+	@Resource
+	private AdService adService;
+	
 	/**
 	 * @param requestData
 	 * @return
 	 * @throws IOException 
 	 */
 	@ResponseBody
-	@RequestMapping(value = "/test", produces = "text/plain;charset=UTF-8")
+	@RequestMapping(value = "/test.shtml", produces = "text/plain;charset=UTF-8")
 	public String adClick(HttpServletRequest rq, String jsonData) throws Exception{
-		Map<String, Object> map = null;
-		try {
-			logger.info("=============uuId=================" + jsonData);
-			map = new HashMap<String, Object>();
-			map.put("jsonData", jsonData);
-			map.put("jsonData1", "jsonData1jsojsonData9jsonData9jsonData9nData1jsonData1");
-			map.put("jsonData4", "jsonData1jsojsonData9jsonData9jsonData9nData1jsonData1");
-			map.put("jsonData5", "jsonData1jsojsonData9jsonData9jsonData9nData1jsonData1");
-			map.put("jsonData6", "jsonData1jsojsonData9jsonData9jsonData9nData1jsonData1");
-			map.put("jsonData7", "jsonData1jsojsonData9jsonData9jsonData9nData1jsonData1");
-			map.put("jsonData2", "jsonData2jsonData2jjsonData9jsonData9sonData2jsonData2jsonData2");
-			map.put("jsonData3", "jsonData3jsonData3jsojsonData9jsonData9jsonData9jsonData9nData3jsonData3");
-			map.put("jsonData9", "jsonData9jsonData9jsonData9jsonData9jsonData9jsonData9jsonData9jsonData9jsonData9jsonData9");
-			Thread.sleep(15);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return JSONUtils.toJSONString(map);
+		System.out.println("=============uuId=================" + jsonData);
+		return "ok";
 	}
+	
+	/**
+	 * @param requestData
+	 * @return
+	 * @throws IOException 
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/test1.shtml", produces = "text/plain;charset=UTF-8")
+	public String adClick1(HttpServletRequest rq, @RequestBody byte[]bytes) throws Exception{
+		System.out.println("=============uuId=================");
+		if(bytes != null && bytes.length != 0 ) {
+			String postStr=new String(bytes, "UTF-8");
+			String postStr1=new String(bytes);
+			System.out.println(URLDecoder.decode(postStr, "UTF-8"));
+			System.out.println(postStr1);
+			String ok = CryptUtils.decrypt(postStr, "zhidian@nextad");
+			System.out.println("ok=" + ok);
+		} else {
+			System.out.println("bytes=" + bytes);
+		}
+		return "ok";
+	}
+	
 	
 }
 
--- src/test/java/com/zhidian3g/nextad/test/SpringServiceTestCase.java
+++ src/test/java/com/zhidian3g/nextad/test/SpringServiceTestCase.java
@@ -12,11 +12,21 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.zhidian3g.common.utils.JsonUtil;
+import com.zhidian3g.nextad.service.AdService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/application-context.xml"})
 public class SpringServiceTestCase extends AbstractJUnit4SpringContextTests {
 	protected Logger logger = LoggerFactory.getLogger("test");
+	@Resource
+	private AdService adService;
 	
+	@Test
+	public void testDataBase() {
+//		System.out.println(adService.getAdById(3l));
+		logger.info(JsonUtil.bean2Json(adService.getAdById(3l))); 
+		logger.debug(JsonUtil.bean2Json(adService.getAdById(3l))); 
+		logger.warn(JsonUtil.bean2Json(adService.getAdById(3l))); 
+	}
 	
 }
